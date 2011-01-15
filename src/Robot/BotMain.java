45e3afbba35cec984523a9b559f8acf8c27aa8c3
chadbarbe <chad@chadbarbe.com>
2011-01-15 16:15:08 -0500

    Fixed 2010 code so that it will run on the 2011 image:
    - Fixed resources/META-INF/MANIFEST.MF from 2010 code so that we didn't get a runtime error.
    - Added simple tank drive and user message to BotMain
    - Created OldBotMain with old 2010 BotMain

--- src/Robot/BotMain.java
+++ src/Robot/BotMain.java
@@ -8,164 +8,51 @@
 package Robot;
 
 
-import edu.wpi.first.wpilibj.CANJaguar;
-import edu.wpi.first.wpilibj.AnalogChannel;
-import edu.wpi.first.wpilibj.AnalogModule;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Dashboard;
-import edu.wpi.first.wpilibj.DigitalModule;
-import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStationLCD;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
+ * functions corresponding to each mode, as described in the SimpleRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class BotMain extends IterativeRobot {
+public class BotMain extends SimpleRobot {
+    private RobotDrive drive = new RobotDrive(1,2);
+    private Joystick leftStick = new Joystick(1);
+    private Joystick rightStick = new Joystick(2);
+    private DriverStationLCD myStationLCD = DriverStationLCD.getInstance();
 
-    private Compressor kickerPower;
-    private Kicker ronaldinho;
-    private int m_pressureSwitchChannel = 2;
-    private int m_compressorRelayChannel = 1;
-    private Joystick driveStick, kickStick;
-    private SingleStickDrive spaceInvaders;
-    private SWCrabDrive drive;
-    private CANJaguar turnMotor;
-    private Autonomous skynet;
-
-    public void robotInit()
+    public BotMain()
     {
-        System.out.println("RobotInit()");
-        kickerPower = new Compressor(m_pressureSwitchChannel, m_compressorRelayChannel);
-        
-        driveStick = new Joystick(1);
-        kickStick = new Joystick(2);
-
-        turnMotor = new CANJaguar(2);
-
-        //spaceInvaders = new SingleStickDrive(driveStick, kickStick);
-        drive = new SWCrabDrive(turnMotor, new AnalogChannel(1), driveStick,
-                                new CANJaguar(5), new CANJaguar(8),
-                                new CANJaguar(7), new CANJaguar(4));
-
-        ronaldinho = new Kicker(kickStick);
-        skynet = new Autonomous(drive, ronaldinho);
-    }
-
-    public void autonomousInit()
-    {
-        kickerPower.start();
-        skynet.reset();
+        drive.setSafetyEnabled(false);
     }
     /**
-     * This function is called periodically during autonomous
+     * This function is called once each time the robot enters autonomous mode.
      */
-    public void autonomousPeriodic()
-    {
-        //skynet.run();
+    public void autonomous() {
+        System.out.println("Autonomous Control");
     }
 
-    public void teleopInit()
-    {
-        ronaldinho.Reset();
-        kickerPower.start();
-    }
     /**
-     * This function is called periodically during operator control
+     * This function is called once each time the robot enters operator control.
      */
-    public void teleopPeriodic()
-    {
-        ronaldinho.Run();
-        //spaceInvaders.Run();
-        drive.update();
-        updateDashboard();
-    }
-
-    public void disabledPeriodic() {}
-
-    public void updateDashboard() {
-        Dashboard lowDashData = DriverStation.getInstance().getDashboardPackerLow();
-        lowDashData.addCluster();
+    public void operatorControl() {
+        System.out.println("Operator Control!");
+        while(true)
         {
-            lowDashData.addCluster();
-            {     //analog modules
-                lowDashData.addCluster();
-                {
-                    for (int i = 1; i <= 5; i++) {
-                        lowDashData.addFloat((float) AnalogModule.getInstance(1).getAverageVoltage(i));
-                    }
-                    lowDashData.addFloat((float)turnMotor.getOutputCurrent());
-                    lowDashData.addFloat((float)turnMotor.getTemperature());
-                    lowDashData.addFloat((float) AnalogModule.getInstance(1).getAverageVoltage(8));
-                }
-                lowDashData.finalizeCluster();
-                lowDashData.addCluster();
-                {
-                    for (int i = 1; i <= 8; i++) {
-                        lowDashData.addFloat((float) AnalogModule.getInstance(2).getAverageVoltage(i));
-                    }
-                }
-                lowDashData.finalizeCluster();
-            }
-            lowDashData.finalizeCluster();
-
-            lowDashData.addCluster();
-            { //digital modules
-                lowDashData.addCluster();
-                {
-                    lowDashData.addCluster();
-                    {
-                        int module = 4;
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
-                        lowDashData.addCluster();
-                        {
-                            for (int i = 1; i <= 10; i++) {
-                                lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
-                            }
-                        }
-                        lowDashData.finalizeCluster();
-                    }
-                    lowDashData.finalizeCluster();
-                }
-                lowDashData.finalizeCluster();
-
-                lowDashData.addCluster();
-                {
-                    lowDashData.addCluster();
-                    {
-                        int module = 6;
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
-                        lowDashData.addByte(DigitalModule.getInstance(module).getRelayReverse());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
-                        lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
-                        lowDashData.addCluster();
-                        {
-                            for (int i = 1; i <= 10; i++) {
-                                lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
-                            }
-                        }
-                        lowDashData.finalizeCluster();
-                    }
-                    lowDashData.finalizeCluster();
-                }
-                lowDashData.finalizeCluster();
-
-            }
-            lowDashData.finalizeCluster();
-
-            lowDashData.addByte(Solenoid.getAll());
+            drive.tankDrive(leftStick, rightStick);
+            Timer.delay(0.005);
         }
-        lowDashData.finalizeCluster();
-        lowDashData.commit();
-
     }
 
+    public void disabled()
+    {
+        myStationLCD.println(DriverStationLCD.Line.kUser2, 1, "LET'S GO XQ!!!!!!");
+        myStationLCD.updateLCD();
+    }
 }
