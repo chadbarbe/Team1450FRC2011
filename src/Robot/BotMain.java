0aa164bc0b333e6a9621909be41722238a047727
chadbarbe <chad@chadbarbe.com>
2011-02-05 13:20:49 -0500

    Fixed velocity control for real this time

--- src/Robot/BotMain.java
+++ src/Robot/BotMain.java
@@ -24,6 +24,7 @@ import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
+import Robot.VelocityDrive;
 
 //import edu.wpi.first.wpilibj.CANJaguar;
 //import edu.wpi.first.wpilibj.AnalogChannel;
@@ -45,17 +46,18 @@ public class BotMain extends SimpleRobot {
     private PIDSource autonomousPIDSourceDistance1;
     private PIDController autonomousPIDDistance1;
     private SpeedController jag1 = new Jaguar(1);
+    private SpeedController jag2 = new Jaguar(2);
     private DrivePIDOutput pidDrive1 = new DrivePIDOutput(jag1, false);
     private Joystick joy1  = new Joystick(1);
+    private Joystick joy2  = new Joystick(2);
     private Encoder encoder2 = new Encoder(1,2);
     private PIDSource autonomousPIDSourceDistance2;
     private PIDController autonomousPIDDistance2;
-    private SpeedController jag2 = new Jaguar(2);
     private DrivePIDOutput pidDrive2 = new DrivePIDOutput(jag2, true);
-    private Compressor fang = new Compressor(12,1);
+    private Compressor windtunnel = new Compressor(12,1);
     private Solenoid sole1 = new Solenoid(1);
     private Solenoid sole2 = new Solenoid(2);
-   
+    private RobotDrive drive = new RobotDrive(jag1,jag2);
    
 
     public BotMain()
@@ -74,14 +76,13 @@ public class BotMain extends SimpleRobot {
      */
     public void autonomous() {
         System.out.println("Autonomous Control");
-        fang.start();
+        windtunnel.start();
         encoder1.setDistancePerPulse(wheelDiameter / ticksPerRev);
         encoder1.reset();
         encoder1.start();
         encoder2.setDistancePerPulse(wheelDiameter / ticksPerRev);
         encoder2.reset();
         encoder2.start();
-        encoder2.setReverseDirection(true);
 
 //        autonomousPIDDistance1.setTolerance(2);
 //        autonomousPIDDistance1.setSetpoint(230);
@@ -93,18 +94,30 @@ public class BotMain extends SimpleRobot {
 //        autonomousPIDDistance2.setOutputRange(-0.7, 0.7);
 //        autonomousPIDDistance2.enable();
 
-//        VelocityDrive myVelocityDrive = new VelocityDrive(encoder2, jag2, 75, DriverStationLCD.Line.kUser2, "1");
-//        myVelocityDrive.start();
-//        myVelocityDrive.setTarget(50);
-//
-//        VelocityDrive myVelocityDrive2 = new VelocityDrive(encoder2, jag2, 75, DriverStationLCD.Line.kUser3, "2");
-//        myVelocityDrive2.start();
-//        myVelocityDrive2.setTarget(-50);
+        VelocityDrive myVelocityDrive = new VelocityDrive(encoder1,
+                jag1,
+                75,
+                true,
+                DriverStationLCD.Line.kUser2,
+                DriverStationLCD.Line.kUser3,
+                DriverStationLCD.Line.kUser4,
+                "1");
+        myVelocityDrive.setTarget(50);
+        myVelocityDrive.start();
 
-//        VelocityDrive myVelocityDrive2 = new VelocityDrive(encoder2, jag2, 75, DriverStationLCD.Line.kUser3, "2");
-//        myVelocityDrive2.start();
-//        myVelocityDrive2.setTarget(-50);
-//
+        VelocityDrive myVelocityDrive2 = new VelocityDrive(encoder2, 
+                jag2,
+                75,
+                false,
+                DriverStationLCD.Line.kUser4,
+                DriverStationLCD.Line.kUser5,
+                DriverStationLCD.Line.kUser6,
+                "2");
+        myVelocityDrive2.setTarget(50);
+        myVelocityDrive2.start();
+
+
+
 //        while(true)
 //        {
 //            myStationLCD.println(DriverStationLCD.Line.kUser2, 1, "E1D = " + encoder1.getDistance());
@@ -119,12 +132,18 @@ public class BotMain extends SimpleRobot {
      */
     public void operatorControl() {
         System.out.println("Operator Control!");
+        encoder1.setDistancePerPulse(wheelDiameter / ticksPerRev);
+        encoder1.reset();
         encoder1.start();
+        encoder2.setDistancePerPulse(wheelDiameter / ticksPerRev);
+        encoder2.reset();
         encoder2.start();
-        fang.start();
+        encoder2.setReverseDirection(true);
+        windtunnel.start();
         while(true)
         {
             Timer.delay(0.005);
+            drive.tankDrive(joy1, joy2);
             myStationLCD.println(DriverStationLCD.Line.kUser2, 1, "E1D = " + encoder1.getDistance());
             myStationLCD.println(DriverStationLCD.Line.kUser2, 1, "E2D = " + encoder2.getDistance());
             
