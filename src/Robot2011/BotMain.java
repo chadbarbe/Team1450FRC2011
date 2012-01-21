772cb56934207c7fccd20c87d7ad1feef6f4e77b
chadbarbe <chad@chadbarbe.com>
2012-01-21 12:39:37 -0500

    added my name to comments to see what branching looks like

--- src/Robot2011/BotMain.java
+++ src/Robot2011/BotMain.java
@@ -3,6 +3,7 @@
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
+/* chad									      */
 /*----------------------------------------------------------------------------*/
 package Robot2011;
 
