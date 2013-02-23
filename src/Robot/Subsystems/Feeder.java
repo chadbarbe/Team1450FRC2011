0804f320822c3e5de13f2035b15e5f7af53ba7b6
Brian Hanna <bri.hanna@gmail.com>
2013-02-23 15:51:40 -0500

    Functional Code

---
+++ src/Robot/Subsystems/Feeder.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Robot.Subsystems;
+
+import Robot.Commands.Feeder.FeederOffCommand;
+import Robot.Utils.Threading;
+import RobotMain.IODefines;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
+/**
+ *
+ * @author keenan
+ */
+public class Feeder extends Subsystem {
+    
+    private Jaguar feederMotor = new Jaguar(IODefines.FEEDER_MOTOR);
+    private DigitalInput feederLimit = new DigitalInput(IODefines.FEEDER_LIMIT_SWITCH);
+    boolean feeding;
+    
+    protected void initDefaultCommand() {
+        setDefaultCommand(new FeederOffCommand());
+    }
+    
+    public void feed() {
+        
+        //feederLimit.
+        feederMotor.set(-.7);
+        System.out.println("Feeder Limit Switch: " + feederLimit.get());
+        if (!feeding){
+            feeding = true;
+            feederMotor.set(-.7);
+            //Threading.sleep(250);
+            while(feederLimit.get()){
+                Threading.sleep(10);
+            }
+            
+            while(!feederLimit.get()){
+                Threading.sleep(10);
+            }
+            feederMotor.set(0);
+            feeding = false;
+        }
+        
+    }
+    
+    public void unfeed()  {
+        feederMotor.set(.7);
+    }
+    
+    private void off()  {
+      //  feederMotor.set(0.0);
+    }
+}
